6f5a7e85a703c68224420e43632d0f2abf9c02e1
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-08 19:35:41 +0300

    Распределение кода js по файлам

---
+++ src/js/setOnloadEvent.js
@@ -0,0 +1,27 @@
+import { addAnimate } from "./addAnimate.js";
+
+// Установка отступа для главной страницы
+
+const main = document.querySelector("#main");
+const header = document.querySelector(".header");
+
+let setPaddingMain = () => {
+  main.style.paddingTop = header.offsetHeight - 1 + "px";
+};
+
+export let setOnloadEvent = () => {
+  window.onload = function () {
+    setPaddingMain();
+    // Убирание прелодера при полной загрузке
+    document
+      .querySelector(".preloader-container")
+      .classList.add("preloader_close");
+
+    // Запуск анимации
+    addAnimate();
+  };
+
+  window.onresize = function () {
+    setPaddingMain();
+  };
+};
