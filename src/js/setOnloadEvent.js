2198788ba27f7305578d573a5433e37d961cce96
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-30 22:55:42 +0300

    Создание меню на 768

--- src/js/setOnloadEvent.js
+++ src/js/setOnloadEvent.js
@@ -1,15 +1,40 @@
 import { addAnimate } from "./addAnimate.js";
 
 // Установка отступа для главной страницы
-
 const main = document.querySelector("#main");
 const header = document.querySelector("#header");
 
-let setPaddingMain = () => {
+const setPaddingMain = () => {
   main.style.paddingTop = header.offsetHeight - 1 + "px";
 };
 
-export let setOnloadEvent = () => {
+// Бургер
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".header__nav");
+const body = document.querySelector("body");
+const items = document.querySelectorAll(".menu__item");
+
+const toggleClasses = () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+  body.classList.toggle("lock");
+};
+
+const setMenuListener = () => {
+  if (document.documentElement.clientWidth <= 768) {
+    burger.addEventListener("click", toggleClasses);
+    for (let item of items) {
+      item.addEventListener("click", toggleClasses);
+    }
+  } else {
+    burger.removeEventListener("click", toggleClasses);
+    for (let item of items) {
+      item.removeEventListener("click", toggleClasses);
+    }
+  }
+};
+
+export const setOnloadEvent = () => {
   window.onload = function () {
     setPaddingMain();
     // Убирание прелодера при полной загрузке
@@ -19,9 +44,14 @@ export let setOnloadEvent = () => {
 
     // Запуск анимации
     addAnimate();
+
+    // Работа с меню
+    setMenuListener();
   };
 
   window.onresize = function () {
     setPaddingMain();
+
+    setMenuListener();
   };
 };
